6c1da2e3826f085176142988b72418e2cc8ac67a
Ali Ibrahim <ali22ibrahim12@gmail.com>
2025-07-08 16:30:32 +0300

    build a periodic table

--- element.sh
+++ element.sh
@@ -36,3 +36,4 @@ IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$ELEMENT"
 
 # Print the formatted output
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+
